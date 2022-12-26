0e9d249067187eb242f02a76938feb5413613998
Nvd <nvdnzai@gmail.com>
2022-12-26 09:16:19 -0500

    update

--- Expert-AI/main.py
+++ Expert-AI/main.py
@@ -1,192 +1,120 @@
 import numpy as np
-from itertools import groupby
-
                                     # start with black nuts
-board = np.array([    ['0' , 'b1' , '0' , '0' , 'q' , '0' , '0' , '0'],
-                      ['0' , '0' , '0' , '0' , '0' , '0' , '0' , '0'],
-                      ['0' , '0' , '0' , '0' , '0' , '0' , '0' , '0'],
-                      ['0' , '0' , 'p' , '0' , 'K' , '0' , 'r' , '0'],
-                      ['0' , '0' , '0' , '0' , '0' , 'p' , '0' , '0'],
-                      ['0' , '0' , '0' , 'kn' , 'p' , '0' , '0' , '0'],
-                      ['0' , '0' , '0' , '0' , 'r' , '0' , '0' , '0'],
-                      ['0' , '0' , '0' , '0' , '0' , '0' , '0' , '0']],  dtype=object)
+board = np.array([    ['0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'],
+                      ['0'  ,  '0'  ,  'p'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'],
+                      ['0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'],
+                      ['0'  ,  '0'  ,  '0'  ,  'K'  ,  '0'  ,  '0'  ,  '0'  ,  '0'],
+                      ['0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'],
+                      ['0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'],
+                      ['0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'],
+                      ['0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0'  ,  '0']],  dtype=object)
                                     # start with white nuts
 print(board)
 
-
 kingsymbol = ["K","k"]
 start_mood = (1,0)  # frist item is black start mood and secend itrm is white start mood (0 is down 1 is top)
-check_count = 0 
 
-
-def check_on_diagonals(board, check_count, symbol, checker_symbols, massage):
-
+def check_on_diagonals(board, symbol, checker_symbols, massage):
     diags = [board[::-1,:].diagonal(i) for i in range(-board.shape[0]+1, board.shape[1])]
     diags.extend(board.diagonal(i) for i in range(board.shape[1]-1,-board.shape[0],-1))
     all_diags = [n.tolist() for n in diags]
 
-    for checker_symbol in checker_symbols:
-        for diag in all_diags:
-
-            if (symbol in diag) and (checker_symbol in diag):
-                kinx = diag.index(symbol)
-                checker_index = diag.index(checker_symbol)
-
-                if kinx > checker_index:
-                    if diag[kinx-1]== checker_symbol:
-                        print(massage,checker_symbol)
-                        check_count +=1 
-                    elif groupby(diag[checker_index+1:kinx]) and ("0" in diag[checker_index+1:kinx] ):
-                        print(massage,checker_symbol)
-                        check_count +=1 
+    checked_by = 0
+    for diag in all_diags:
+        check_list = [-1]
+        if (symbol in diag) and len(list(set(diag) & set(checker_symbols)))>0:
+            for item in diag:
+                if item == "0":
+                    pass
                 else:
-                    if diag[kinx+1]== checker_symbol:
-                        print(massage,checker_symbol)
-                        check_count +=1 
-                    elif groupby(diag[kinx+1:checker_index]) and ("0" in diag[kinx+1:checker_index] ):
-                        print(massage,checker_symbol)
-                        check_count +=1 
-    return check_count
+                    check_list.append(item)
+                    look = (check_list[-2],check_list[-1])
+                    if (look[0] in checker_symbols) and (look[1] == symbol):
+                        checked_by = look[0]
+                        print(massage,checked_by)
+                        break 
+                    elif (look[1] in checker_symbols) and (look[0] == symbol):                  
+                        checked_by = look[1]
+                        print(massage,checked_by)
+                        break
+                    else:
+                        pass
+        elif checked_by != 0:
+            break
+        else:
+            pass 
+    return checked_by
+  
+def check_on_rowcols(board, symbol, checker_symbols, massage):
+    king_indx = np.where(board == symbol)
+    row = board[king_indx[0]]
+    col = board[:,king_indx[1]]
+    col = np.reshape(col,(1,8))
 
-def check_on_rowcols(board, check_count, symbol, checker_symbol, massage):
+    checked_by = 0
+    side = [(row[0], "on row by "),(col[0],"on col by")]
 
-    sid = ["on row","on col"]
-    for counter,i in  enumerate(sid):  # checking for row and col
-        king_indx = np.where(board == symbol)
-        row = board[king_indx[0]]
-        if counter == 1:
-            row = board[: ,king_indx[1]]
-            row = np.reshape(board[: ,king_indx[1]],(1,8))
-
-        rook_indx = np.where(row == checker_symbol)
-        rook_num = len(rook_indx[1])
-        king_indx = np.where(row == symbol)
-
-        if rook_num == 0:
-            pass
-        elif (rook_num == 1) and (rook_indx[1][0] < king_indx[1][0]):
-            if row[king_indx[0][0]][king_indx[1][0]-1] == checker_symbol:
-                check_count+=1
-                print(massage,i)
+    for i,j in side:
+        check_list = [-1]
+        for item in i:
+            if item == "0":
+                pass
             else:
-                dist = row[:,rook_indx[1][0]+1 : king_indx[1][0]].flatten()
-                ln_dist = len(set(dist))
-                if (ln_dist==1) and ("0" in dist):
-                    check_count+=1
-                    print(massage,i)
+                check_list.append(item)
+                look = (check_list[-2],check_list[-1])
+                if (look[0] in checker_symbols) and (look[1] == symbol):
+                    checked_by = look[0]
+                    print(massage,j,checked_by)
+                    break 
+                elif (look[1] in checker_symbols) and (look[0] == symbol):                  
+                    checked_by = look[1]
+                    print(massage,j,checked_by)
+                    break
+                else:
+                    pass
+        if checked_by != 0:
+            break
+    return  checked_by
 
-        elif (rook_num == 1) and (rook_indx[1][0] > king_indx[1][0]):
-            if row[king_indx[0][0]][king_indx[1][0]+1] == checker_symbol:
-                check_count+=1
-                print(massage,i)
-            else:
-                dist = row[:,king_indx[1][0]+1 : rook_indx[1][0]].flatten()
-                ln_dist = len(set(dist))
-                if (ln_dist==1) and ("0" in dist):
-                    check_count+=1
-                    print(massage,i)
-
-        elif (rook_num == 2) and (max(rook_indx[1]) < king_indx[1][0]):
-            if row[king_indx[0][0]][king_indx[1][0]-1] == checker_symbol:
-                check_count+=1
-                print(massage,i)
-            else:
-                dist = row[:,max(rook_indx[1])+1 : king_indx[1][0]].flatten()
-                ln_dist = len(set(dist))
-                if (ln_dist==1) and ("0" in dist):
-                    check_count+=1
-                    print(massage,i)
-
-        elif (rook_num == 2) and (min(rook_indx[1]) > king_indx[1][0]):
-            if row[king_indx[0][0]][king_indx[1][0]+1] == checker_symbol:
-                check_count+=1
-                print(massage,i)
-            else:
-                dist = row[:,king_indx[1][0]+1 : min(rook_indx[1])].flatten()
-                ln_dist = len(set(dist))
-                if (ln_dist==1) and ("0" in dist):
-                    check_count+=1
-                    print(massage,i)
-        
-        elif (rook_num == 2) and (min(rook_indx[1]) < king_indx[1][0] < max(rook_indx[1])):
-            left = row[:,min(rook_indx[1]) : king_indx[1][0]].flatten()
-            ln_left = len(set(left))
-            if (ln_left == 1) or (ln_left==2  and "0" in left):
-                check_count+=1
-                print(massage,i)
-            right = row[:,king_indx[1][0] : max(rook_indx[1])].flatten()
-            ln_right = len(set(right))
-            if (ln_right == 1) or (ln_right==2  and "0" in right):
-                check_count+=1
-                print(massage,i) 
-    return check_count
-
-
-def is_check_rook(board, check_count, symbol):
+def is_check_queen(board, symbol):
     if symbol == "K" :
-        rook_symbol = "r"
+        checker_symbols = ["q1","q2","q3","q4","q5","q6","q7","q8","q9",]
+        massage = "black king checked by white queen "
+    else:
+        checker_symbols = ["Q1","Q2","Q3","Q4","Q5","Q6","Q7","Q8","Q9",]
+        massage = "white king checked by black queen "  
+    checked_by  = check_on_rowcols(board, symbol, checker_symbols, massage) 
+    if checked_by == 0:
+        checked_by  = check_on_diagonals(board, symbol, checker_symbols, massage)   
+    return checked_by 
+
+def is_check_rook(board, symbol):
+    if symbol == "K" :
+        checker_symbols = ["r1","r2","r3","r4","r5","r6","r7","r8","r9","r10"]
         massage = "black king checked by white rook "
     else:
-        rook_symbol = "R"
+        checker_symbols = ["R1","R2","R3","R4","R5","R6","R7","R8","R9","R10"]
         massage = "Wihte king checked by black rook " 
-    check_count = check_on_rowcols(board, check_count, symbol, rook_symbol, massage)
-    return check_count  
+    checked_by = check_on_rowcols(board, symbol, checker_symbols, massage)
+    return  checked_by  
 
-def is_check_pawn(board, check_count, start_mood, symbol ):
-    if symbol == "K":
-        if start_mood[0] == 1:
-            bound = (6,7)
-            i,j = (1,1)
-        else:
-            bound = (0,1)
-            i,j = (-1,1)  
-
-        pawn = "p"
-        king_color = "black king"
-        pawn_color = "white pawn"
-
-    elif symbol == "k":
-        if start_mood[1] == 1:
-            bound = (6,7)
-            i,j = (1,1)
-        else:
-            bound = (0,1)
-            i,j = (-1,1)  
-   
-        pawn = "P"
-        king_color = "white king"
-        pawn_color = "black pawn"
-
-    king_indx = np.where(board == symbol)
-    king_row, king_col = king_indx[0][0],king_indx[1][0]
-    # black king in white start region
-    if (king_row == bound[0]) or (king_row == bound[1]):
-        print(king_color ," is in opponent's starting area ")
-    # black king in saids (col 0)
-    elif (king_col == 1)  and (board[king_row +i][king_col +j] == pawn):
-        print(king_color, "checked by",pawn_color)
-        check_count += 1
-    # black king in saids (col 7)
-    elif (king_col == 7)  and (board[king_row +i][king_col -j] == pawn):
-        print(king_color, "checked by",pawn_color)
-        check_count += 1    
-    # black king in other locations
-    elif (board[king_row +i][king_col +j] == pawn) or (board[king_row +i][king_col -j] == pawn):
-        print(king_color, "checked by",pawn_color)
-        check_count += 1
+def is_check_bishop(board, symbol):
+    if symbol == "K" :
+        checker_symbols = ("b1","b2","b3","b4","b5","b6","b7","b8","b9","b10")
+        massage = "black king checked by white bishop "
     else:
-        print(king_color ," dont have check by ", pawn_color )
-    return check_count
+        checker_symbols = ("B1","B2","B3","B4","B5","B6","B7","B8","B9","B10")
+        massage = "Wihte king checked by black bishop "      
+    checked_by = check_on_diagonals(board, symbol, checker_symbols, massage)
+    return checked_by
 
-def is_check_knight(board, check_count, symbol):
-
+def is_check_knight(board, symbol):
     if symbol == "K":
-        knight_symbol = "kn"
+        checker_symbols = ["kn1","kn2","kn3","kn4","kn5","kn6","kn7","kn8","kn9","kn10"]
         massage = "Black king checked by white knight"
     else :
-        knight_symbol = "KN"
+        checker_symbols = ["KN1","KN2","KN3","KN4","KN5","KN6","KN7","KN8","KN9","KN10"]
         massage = "White king checked by black knight"
-
 
     new_board = np.pad(board, 2)
     king_indx = np.where(new_board == symbol)
@@ -199,68 +127,104 @@ def is_check_knight(board, check_count, symbol):
     corners_1 = rectangle_1[corners_indx]
     corners_2 = rectangle_2[corners_indx]
     all_corners = np.concatenate((corners_1, corners_2), axis=0)
-    knight_indx = np.where(all_corners == knight_symbol)
 
-    if len(knight_indx[0]) == 0:
-        pass
-    else:
+    intersection = list (set ( all_corners.flatten() ) & set(checker_symbols))
+    if len(intersection) > 0:
         print(massage)
-        check_count +=1
-    return check_count
-
-def is_check_bishop(board, check_count, symbol):
-    if symbol == "K" :
-        bishop_symbol = ("b1","b2")
-        massage = "black king checked by white bishop "
+        chekecd_by = intersection[0]
     else:
-        bishop_symbol = ("B1","B2")
-        massage = "Wihte king checked by black bishop "   
-        
-    check_count = check_on_diagonals(board, check_count, symbol, bishop_symbol, massage)
-    return check_count
+        pass 
+    return chekecd_by
 
-def is_check_queen(board, check_count, symbol):
-    if symbol == "K" :
-        queen_symbol = ("q")
-        massage = "black king checked by white queen "
-    else:
-        queen_symbol = ("Q")
-        massage = "white king checked by black queen "  
+def is_check_pawn(board, symbol, start_mood):
+    new_board = np.pad(board,((0,0),(1,1)))
+    king_indx = np.where(new_board == symbol)
+    king_row, king_col = king_indx[0][0],king_indx[1][0]
+    checked_by = 0
 
-    check_count = check_on_diagonals(board, check_count, symbol, queen_symbol, massage)
-    check_count = check_on_rowcols(board, check_count, symbol, queen_symbol, massage)    
-    return check_count
+    if (symbol == "K")  and  (start_mood[0] == 1):
+        checker_symbols = ["p1","p2","p3","p4","p5","p6","p7","p8"]
+        massage = "Black king checked by white pawn"
+        LD = new_board[king_row+1][king_col-1]
+        RD = new_board[king_row+1][king_col+1]
+        if LD in checker_symbols:
+            checked_by = LD
+            print(massage,LD)
+        elif RD in checker_symbols:
+            checked_by = RD
+            print(massage,RD)
+        else:
+            pass
 
+    elif (symbol == "K")  and  (start_mood[0] == 0):
+        checker_symbols = ["p1","p2","p3","p4","p5","p6","p7","p8"]
+        massage = "Black king checked by white pawn by"
+        LU = new_board[king_row-1][king_col-1]
+        RU = new_board[king_row-1][king_col+1]
+        if LU in checker_symbols:
+            checked_by = LU
+            print(massage,LU)
+        elif RU in checker_symbols:
+            checked_by = RU
+            print(massage,RU)
+        else:
+            pass
+    
+    elif (symbol == "k")  and  (start_mood[0] == 1):
+        checker_symbols = ["P1","P2","P3","P4","P5","P6","P7","P8"]
+        massage = "White king checked by black pawn by"   
+        LD = new_board[king_row+1][king_col-1]
+        RD = new_board[king_row+1][king_col+1]
+        if LD in checker_symbols:
+            checked_by = LD
+            print(massage,LD)
+        elif RD in checker_symbols:
+            checked_by = RD
+            print(massage,RD)
+        else:
+            pass
+    
+    elif (symbol == "k")  and  (start_mood[0] == 1):
+        checker_symbols = ["p1","p2","p3","p4","p5","p6","p7","p8"]
+        massage = "Black king checked by white pawn by"
+        LU = new_board[king_row-1][king_col-1]
+        RU = new_board[king_row-1][king_col+1]
+        if LU in checker_symbols:
+            checked_by = LU
+            print(massage,LU)
+        elif RU in checker_symbols:
+            checked_by = RU
+            print(massage,RU)
+        else:
+            pass       
+    return checked_by    
 
+checkers_list = []
 for symbol in kingsymbol:
     print("=======================")
+    checked_by = is_check_queen(board, symbol)
+    checkers_list.append(checked_by)
+    
+    checked_by = is_check_rook(board, symbol)
+    checkers_list.append(checked_by)
+    if len(checkers_list)==2:
+        break
 
-    check_count_all = 0
-    while True:
-        check_count = is_check_rook(board, check_count, symbol)
-        check_count_all = check_count
-     
-        
-        check_count = is_check_pawn(board, check_count, start_mood, symbol)
-        check_count_all += check_count
-        if check_count_all > 2:
-              break
-        
-        check_count = is_check_knight(board, check_count, symbol)
-        check_count_all += check_count
-        if check_count_all > 2:
-              break
-        
-        check_count = is_check_bishop(board, check_count, symbol)
-        check_count_all += check_count
-        if check_count_all > 2:
-              break
-        
-        check_count = is_check_queen(board, check_count, symbol)
-        check_count_all += check_count
-        if check_count_all > 2:
-              break
+    checked_by = is_check_bishop(board, symbol)
+    checkers_list.append(checked_by)
+    if len(checkers_list)==2:
+        break    
 
-    print(check_count)
-    check_count=0
-    qq
+    checked_by = is_check_knight(board, symbol)
+    checkers_list.append(checked_by)
+    if len(checkers_list)==2:
+        break 
+    
+    checked_by = is_check_pawn(board, symbol, start_mood)
+    checkers_list.append(checked_by)
+    if len(checkers_list)==2:
+        break 
+
+    if len(checkers_list) > 0:
+        print("we have ",len(checkers_list), "check")
+        break
